b78e1153898c822af791c40ada1e698bc60e0aa6
Eder Eduardo <eder.esilva@gmail.com>
2011-09-22 11:57:26 -0300

    Edited EncoderFFmpeg.php via GitHub

--- EncoderFFmpeg.php
+++ EncoderFFmpeg.php
@@ -10,9 +10,6 @@
 * @version 0.0.4 
 Exemplo de uso:
 $encoder		= new Encoder();
-$rpl = $encoder->replace("/tv/Portal$item[alta]");  //  
-$encoder->encoderFile($rpl, "$item[ID]") ;         //
-$encoder->ScreenShot()
 
 * @changelog  
 1. Criada a Class Encoder para dar mais escalabilidade ao código 
@@ -20,12 +17,12 @@ $encoder->ScreenShot()
 3. A função encoderFile  foi modificada anteriormente a função recebia um argumento de entrada e saida agora a função recebe somente um argumento de entrada @InFile e outro de @ID  paramentros passado via consulta SQL.
 4. GetSize nova função criada na pega o tamanho do arquivo e verifica se ele realmete foi criado o comparando com original.
 5. Verficação de condição if verificando se o arquivo gerado é realmente menor que o arquivo original.
-6. Criada a função FlvTool para indexar keyframe dentro do vídeo. 23/10/2010
+
 7. Criada a função ScreenShot para gerar imagems dos vídeos particionados por tempo. 
 8. Criar funcão para cortar trechos do vídeos em novos arquivos.
 */
 
- $flvPath    = "/usr/bin/flvtool2" ;					// Caminho do  Executável
+
  $ffmpegPath = "/usr/share/ffmpeg/ffmpeg"; // Caminho do Executável
 
 global $flvPath   ;
@@ -55,10 +52,7 @@ class  Video {
 		else { exec("$ffmpegPath -y -ss $StartTime -t 1 -i $InFile -f mjpeg $imagem" ) ; }
 	}
 
-	function FlvTool($InFile){
-		exec("$flvPath -UP $InFile")  ;   
-		return  $InFile ;
-	}
+
 
 	function GetSize($SizeFile){                // Pega o Tamanho do Arquivo
 		if(is_file($SizeFile)){
