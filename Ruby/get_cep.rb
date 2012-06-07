14d3ba56235ce4d6af38d7be9ccd5178de723040
Eder <eder.esilva@gmail.com>
2012-06-07 12:54:16 -0300

    Removed Key

--- Ruby/get_cep.rb
+++ Ruby/get_cep.rb
@@ -3,8 +3,8 @@ require 'net/http'
 require 'uri'
 
 def cep
- zipcode = '08235770'
- key = "14sSTUEmctBT0Aec56EskBBUVHSck31"
+ zipcode = ''
+ key = ""
  api = "http://www.buscarcep.com.br/?cep=#{zipcode}&formato=xml&chave=#{key}"
  response  = Net::HTTP.get_response(URI.parse(api))
  result = response.body
